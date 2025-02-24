c7b04b9be0229f1b5fd2ad06a908d5225ca8f080
newgolan <993070367@qq.com>
2025-02-24 22:57:23 +0800

    提交修改

--- vite.config.js
+++ vite.config.js
@@ -3,22 +3,43 @@ import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path'
 
 // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [vue()],
+//   resolve: {
+//     alias: {
+//       '@': resolve(__dirname, 'src')
+//     }
+//   },
+//   server: {
+//     port: 3000,
+//     open: true,
+//     proxy: {
+//       '/api': {
+//         target: 'http://localhost:8080',
+//         changeOrigin: true,
+//         rewrite: (path) => path.replace(/^\/api/, '')
+//       }
+//     }
+//   }
+// })
 export default defineConfig({
-  plugins: [vue()],
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/zhuangxiu-AI-Vue/'  // 仓库名称（若部署到子路径）
+    : '/',
+    plugins: [vue()],
   resolve: {
     alias: {
       '@': resolve(__dirname, 'src')
     }
   },
-  server: {
-    port: 3000,
-    open: true,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8080',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
-      }
-    }
+  plugins: [vue()],
+  build: {
+    outDir: 'dist',        // 必须与 gh-pages 的 -d 参数一致
+    emptyOutDir: true      // 构建前清空输出目录
   }
 })
+// module.exports = {
+//   publicPath: process.env.NODE_ENV === 'production'
+//     ? '/zhuangxiu-AI-Vue/'  // 仓库名称（若部署到子路径）
+//     : '/'
+// }
